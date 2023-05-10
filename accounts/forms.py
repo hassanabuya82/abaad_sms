667e4ac1afa1f77bb7c3c97f32cd5d2ea42db9a6
Hassan_codes <109627448+hassanabuya82@users.noreply.github.com>
2023-05-10 17:36:40 +0300

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django import forms 
+from django import forms  
 from django.db import transaction
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 # from django.contrib.auth.models import User
